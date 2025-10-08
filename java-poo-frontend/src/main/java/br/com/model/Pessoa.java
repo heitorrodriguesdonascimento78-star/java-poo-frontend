e28c5d880b2788e54b73416c08b665cd00808153
Heitor Rodrigues <heitorrodriguesdonascimento78@gmail.com>
2025-10-07 22:29:52 -0300

    Add files via upload

---
+++ java-poo-frontend/src/main/java/br/com/model/Pessoa.java
@@ -0,0 +1,11 @@
+package br.com.model;
+
+public class Pessoa {
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private String endereco;
+
+}
